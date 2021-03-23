dc7beeb04a6eca836e86d672a9750635d6354501
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-03-23 23:04:28 +0000

    Adds remaining hyperparameters

--- hps.py
+++ hps.py
@@ -2,6 +2,15 @@ from types import SimpleNamespace
 
 HPS = {
     'ffhq1024': SimpleNamespace(**{
+        'in_channels':              3,
+        'hidden_channels':          128,
+        'res_channels':             32,
+        'nb_res_layers':            2,
+        'nb_levels':                3,
+        'embed_dim':                64,
+        'nb_entries':               512,
+        'scaling_rates':            [4, 4, 2],
+
         'learning_rate':            1e-4,
         'beta':                     0.25,
         'batch_size':               4,
