4a4d952796833115dc992edc1d00eaeb1f3c3093
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-04-27 10:24:04 +0100

    fixes half open range error

--- datasets.py
+++ datasets.py
@@ -8,7 +8,7 @@ def get_dataset(task: str, cfg, shuffle_test=False):
             torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
         ])
         dataset = torchvision.datasets.ImageFolder('data/ffhq1024', transform=transforms)
-        train_idx, test_idx = torch.arange(0, 60_000 - 1), torch.arange(60_000, len(dataset))
+        train_idx, test_idx = torch.arange(0, 60_000), torch.arange(60_000, len(dataset))
         train_dataset, test_dataset = torch.utils.data.Subset(dataset, train_idx), torch.utils.data.Subset(dataset, test_idx)
     elif task == 'ffhq256':
         transforms = torchvision.transforms.Compose([
@@ -17,7 +17,7 @@ def get_dataset(task: str, cfg, shuffle_test=False):
             torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
         ])
         dataset = torchvision.datasets.ImageFolder('data/ffhq1024', transform=transforms)
-        train_idx, test_idx = torch.arange(0, 60_000 - 1), torch.arange(60_000, len(dataset))
+        train_idx, test_idx = torch.arange(0, 60_000), torch.arange(60_000, len(dataset))
         train_dataset, test_dataset = torch.utils.data.Subset(dataset, train_idx), torch.utils.data.Subset(dataset, test_idx)
     elif task == 'ffhq128':
         transforms = torchvision.transforms.Compose([
@@ -25,7 +25,7 @@ def get_dataset(task: str, cfg, shuffle_test=False):
             torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
         ])
         dataset = torchvision.datasets.ImageFolder('data/ffhq128', transform=transforms)
-        train_idx, test_idx = torch.arange(0, 60_000 - 1), torch.arange(60_000, len(dataset))
+        train_idx, test_idx = torch.arange(0, 60_000), torch.arange(60_000, len(dataset))
         train_dataset, test_dataset = torch.utils.data.Subset(dataset, train_idx), torch.utils.data.Subset(dataset, test_idx)
     elif task == 'cifar10':
         transforms = torchvision.transforms.Compose([
